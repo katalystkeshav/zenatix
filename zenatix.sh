c6421b22f703309336b277e205b69526381e77db
katalyst <keshav.thakur998@gmail.com>
2018-07-31 21:29:40 +0530

    added ram usage

--- zenatix.sh
+++ zenatix.sh
@@ -8,11 +8,17 @@ CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | \
            sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
            awk '{print 100 - $1}')
 
+RAM_USAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+		   
 echo $CPU_USAGE
+
+echo $RAM_USAGE
 
 #influx INSERT cpu,host=serverA value=$CPU_USAGE exit
 
 influx -database 'monitor' -execute 'INSERT CPU_Usage,host=serverA value'=$CPU_USAGE
+
+influx -database 'monitor' -execute 'INSERT RAM_Usage,host=serverA value'=$RAM_USAGE
 
 sleep 300
 
